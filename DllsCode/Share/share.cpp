d0f1e56e9349ece38d0e2cc1ba94837b77a505d9
kirillkuks <60854701+kirillkuks@users.noreply.github.com>
2020-11-30 12:14:28 +0200

    Add files via upload

---
+++ DllsCode/Share/share.cpp
@@ -0,0 +1,24 @@
+#include "share.h"
+
+Parser* getParser() {
+	return new ShareParser();
+}
+
+Share::Share() {
+	left = nullptr;
+	right = nullptr;
+}
+
+double Share::calculate() {
+	return left->calculate() / right->calculate();
+}
+
+int Share::priority() {
+	return 2;
+}
+
+ShareParser::ShareParser() : Parser("/") {}
+
+Expression* ShareParser::parse() {
+	return new Share();
+}
---
+++ DllsCode/Share/share.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "operations.h"
+
+#ifdef DLLSHARE_EXPORTS
+#define DLLMINUS_API __declspec(dllexport)
+#else
+#define DLLMINUS_API __declspec(dllimport)
+#endif
+
+using namespace operations;
+
+extern "C" DLLMINUS_API Parser * getParser();
+
+struct Share : BinaryOperation {
+	Share();
+	double calculate() override;
+	int priority() override;
+};
+
+struct ShareParser : Parser {
+	ShareParser();
+	Expression* parse() override;
+};
